f76ac79655ba856cbc5a719243ffc2f06c9f6291
d4089663 <msendinv@gmail.com>
2025-02-19 12:07:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+This Android app allows users to retrieve the current latitude and longitude of their device using Kotlin and Jetpack Compose.
+
+<B>Features:</B><br>
+Get Current Location: Users can easily obtain the latitude and longitude coordinates of their device by simply clicking a button.
+Permission Handling: The app handles location permissions gracefully, requesting the necessary permissions from the user if they haven't been granted already.
+Jetpack Compose UI: The user interface is built entirely using Jetpack Compose, offering a modern and declarative approach to UI development.
+
+<B>How to Use:</B><br>
+Add your API key in Manifest file
+Launch the app on your Android device.
+Grant the app permission to access your device's location if prompted.
+Click the "Get Location" button to retrieve the current latitude and longitude coordinates of your device.
+
+<B>Technologies Used:</B><br>
+Kotlin
+Jetpack Compose
+Google Map API
+Android Permissions API
+<br><br>
+![locationimage](https://github.com/Swapnil-J-Patil/CurrentLocationApp/assets/129786110/f863cd5c-530d-4970-9124-6bb6aa72d6ac)
---
+++ app/src/main/java/com/example/basiclocationsamplecompose/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.example.basiclocationsamplecompose
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.MapsInitializer
+import com.swapnil.basiclocationsamplecompose.ui.theme.BasicLocationComposeTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var locationRequired: Boolean = false
+    private val permissions=arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+    )
+
+    private var mCurrentLastLocationLatitude = mutableStateOf("0.0")
+    private var mCurrentLastLocationLongitude = mutableStateOf("0.0")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MapsInitializer.initialize(this,MapsInitializer.Renderer.LATEST){
+
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        setContent {
+            BasicLocationComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    shape = RoundedCornerShape(16.dp), //MaterialTheme.shapes.medium,
+                    tonalElevation = 5.dp,
+                    contentColor = Color.Blue,
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LocationScreen(this@MainActivity, mCurrentLastLocationLatitude, mCurrentLastLocationLongitude)
+                }
+            }
+        }
+    }
+
+ fun getLastLocation()
+ {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) return
+
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                // Got last known location. In some rare situations this can be null
+                mCurrentLastLocationLatitude.value = (location?.latitude?: 0.0).toString()
+                mCurrentLastLocationLongitude.value = (location?.longitude?: 0.0).toString()
+            }
+                .addOnFailureListener {
+                    Toast.makeText(this,getString(R.string.failed), Toast.LENGTH_SHORT).show()
+                }
+}
+
+
+    @Composable
+    private fun LocationScreen(
+        context: Context,
+        currentLatitude: MutableState<String>,
+        currentLongitude: MutableState<String>,
+        ) {
+        val launchMultiplePermissions= rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions() )
+        {
+                permissionMaps->
+            val areGranted = permissionMaps.values.reduce{acc,next->acc && next}
+            if (areGranted)
+            {
+                locationRequired = true
+                getLastLocation()
+                Toast.makeText(context,"Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(context,"Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        LaunchedEffect(Unit){
+            if (permissions.all {
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        it
+                    ) == PackageManager.PERMISSION_GRANTED
+                }) {
+                getLastLocation()
+            } else {
+                launchMultiplePermissions.launch(permissions)
+            }
+        }
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Your location is ${(currentLatitude.value)} and ${(currentLongitude.value)}",
+                    color = Color.Black)
+                Button(onClick = {
+                    if (permissions.all {
+                            ContextCompat.checkSelfPermission(
+                                context,
+                                it
+                            ) == PackageManager.PERMISSION_GRANTED
+                        }) {
+                        getLastLocation()
+                    } else {
+                        launchMultiplePermissions.launch(permissions)
+                    }
+                }) {
+                    Text(text = "Check permission and try again")
+                }
+            }
+    }
+}
+
